0f5900f65f8641fe34f3186b226aa08eecfe5bea
syedakhterdev <dev.syed.akhter@gmail.com>
2019-05-08 13:37:13 -0700

    Updates

---
+++ precache-manifest.0d0f3ab5adb3aea971be200f5373bdfa.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "e47ea4c0b9151fe6cbd8e2f332113559",
+    "url": "/pickAspot/static/media/CircularStd-Medium.e47ea4c0.woff"
+  },
+  {
+    "revision": "eb67bc6c16ce0e179065",
+    "url": "/pickAspot/static/css/main.61704cc6.chunk.css"
+  },
+  {
+    "revision": "75605a4995014da5333b",
+    "url": "/pickAspot/static/js/runtime~main.75605a49.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pickAspot/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3cef6d52815cdbfb5234",
+    "url": "/pickAspot/static/js/2.3cef6d52.chunk.js"
+  },
+  {
+    "revision": "b88d7b4e2abab9df7bbd23902aa42bbf",
+    "url": "/pickAspot/static/media/CircularStd-BoldItalic.b88d7b4e.woff2"
+  },
+  {
+    "revision": "0afc4e786e3062a37ea255d7acefba28",
+    "url": "/pickAspot/static/media/CircularStd-Medium.0afc4e78.woff2"
+  },
+  {
+    "revision": "35e6fb86d66a830d45e377494420c11b",
+    "url": "/pickAspot/static/media/CircularStd-Book.35e6fb86.woff2"
+  },
+  {
+    "revision": "8b30196cf402e1b9062b8d67d4d685a0",
+    "url": "/pickAspot/static/media/CircularStd-BookItalic.8b30196c.woff2"
+  },
+  {
+    "revision": "b26584c8d17834161eb3460b00f2affd",
+    "url": "/pickAspot/static/media/CircularStd-Bold.b26584c8.woff2"
+  },
+  {
+    "revision": "6da584bdd8de6496ee54c90ea06aaeaf",
+    "url": "/pickAspot/static/media/CircularStd-MediumItalic.6da584bd.woff2"
+  },
+  {
+    "revision": "a7d0eebb9d2e5cc322735d10e8b7f95c",
+    "url": "/pickAspot/static/media/CircularStd-Black.a7d0eebb.woff2"
+  },
+  {
+    "revision": "8f80cd37b0c3703245260474f4bc89a8",
+    "url": "/pickAspot/static/media/CircularStd-BlackItalic.8f80cd37.woff2"
+  },
+  {
+    "revision": "eb67bc6c16ce0e179065",
+    "url": "/pickAspot/static/js/main.eb67bc6c.chunk.js"
+  },
+  {
+    "revision": "6f4fe3ca482221e7c0d8c1baa5f31100",
+    "url": "/pickAspot/static/media/CircularStd-Bold.6f4fe3ca.woff"
+  },
+  {
+    "revision": "cc06409e5a820f455b3b4883fbf9b72f",
+    "url": "/pickAspot/static/media/CircularStd-BookItalic.cc06409e.woff"
+  },
+  {
+    "revision": "c47d7e581f2a563f8dedd4509c106fb8",
+    "url": "/pickAspot/static/media/CircularStd-Book.c47d7e58.woff"
+  },
+  {
+    "revision": "b5fa1b553516d1cf8d03d6b4588058a0",
+    "url": "/pickAspot/static/media/CircularStd-Black.b5fa1b55.woff"
+  },
+  {
+    "revision": "8d5dbe8149820b1202cbaf34b7204598",
+    "url": "/pickAspot/static/media/CircularStd-BoldItalic.8d5dbe81.woff"
+  },
+  {
+    "revision": "eddb70218f38749eaeeed7fba894fda8",
+    "url": "/pickAspot/static/media/CircularStd-MediumItalic.eddb7021.woff"
+  },
+  {
+    "revision": "2153a12d1db4eda5b954e54532175e5d",
+    "url": "/pickAspot/static/media/CircularStd-BlackItalic.2153a12d.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pickAspot/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pickAspot/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pickAspot/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pickAspot/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3cef6d52815cdbfb5234",
+    "url": "/pickAspot/static/css/2.4a6fe2ea.chunk.css"
+  },
+  {
+    "revision": "30b85965712f9d8b642185a087375597",
+    "url": "/pickAspot/index.html"
+  }
+];
